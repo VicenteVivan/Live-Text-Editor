5fae3700442b6dde2aa55290dccea93b9f997586
VicenteVivan <vicente.vivancoc@gmail.com>
2021-03-12 12:10:01 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const socket = io("ws://localhost:3000", { transports: ["websocket", "polling", "flashsocket"] });
+
+const usernameInput = document.querySelector("input");
+const textBox = document.querySelector("textarea");
+const writingUser = document.querySelector("span");
+
+textBox.addEventListener("keyup", (e) => {
+    if (usernameInput.value != "") {
+        socket.emit("new:text", { name: usernameInput.value, text: textBox.value });
+    } else {
+        setTimeout(() => {
+            textBox.value = textBox.value = "";
+        }, 1);
+    }
+});
+
+socket.on("new:text", (data) => {
+    textBox.value = data.text;
+    console.log(data.name + " is writing");
+});
